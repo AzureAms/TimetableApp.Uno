fd9ad4b815bf1feacf6fd2c49db6665363a1a14f
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2022-02-09 19:26:48 +0700

    ci: Fix .NET 6.0 builds

--- TimetableApp/TimetableApp.Wasm/CopyHtmlScript.ps1
+++ TimetableApp/TimetableApp.Wasm/CopyHtmlScript.ps1
@@ -1,6 +1,6 @@
 # Fix for Github Pages. Local builds do not need this fix.
 
-$s = Get-ChildItem -Name -Directory ($PSScriptRoot + "\bin\Release\net5.0\publish\package*"); 
-$htmlFile = $PSScriptRoot + "\bin\Release\net5.0\publish\" + $s + "\index.html";
+$s = Get-ChildItem -Name -Directory ($PSScriptRoot + "\bin\Release\net6.0\publish\package*"); 
+$htmlFile = $PSScriptRoot + "\bin\Release\net6.0\publish\" + $s + "\index.html";
 echo ("Copying file to " + $PSScriptRoot + $htmlFile);
 Copy-Item ($PSScriptRoot + "\package_index.html") $htmlFile
